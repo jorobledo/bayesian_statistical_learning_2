5c85c6acfa3bb462b9f6b8c32acca761da60f076
Steve Schmerler <git@elcorto.com>
2025-05-14 16:32:40 +0200

    Improve py -> ipynb conversion

--- BLcourse2.3/convert-to-ipynb.sh
+++ BLcourse2.3/convert-to-ipynb.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Remove plots etc, but keep random cell IDs. We need this b/c we use jupytext
+# --update below.
+nbstripout --keep-id *.ipynb
+
 for fn in 0*dim.py; do
+    # --update keeps call IDs -> smaller diffs
     jupytext --to ipynb --update $fn
 done
